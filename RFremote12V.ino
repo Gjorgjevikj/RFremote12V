5616b83b95c438ffe24c42a14529c7c797eaa14e
Gjorgjevikj <dejan.gjorgjevikj@gmail.com>
2021-04-20 02:57:26 +0200

    Support for second type of RF code.

--- RFremote12V.ino
+++ RFremote12V.ino
@@ -10,7 +10,7 @@
 #include <EEWrap.h>
 #include "rfcodes.h"
 
-#define RF_TX_PIN 0 // the TxPin applied to the 433MHz modulator pin
+#define RF_TX_PIN 0 // the RF_TX_PIN applied to the 433MHz modulator pin
 #define DIP1_PIN 1 // DIPswitch/jumper pin 1
 #define DIP2_PIN 2 // DIPswitch/jumper pin 2
 
@@ -41,7 +41,6 @@ SoftwareSerial MySerial(RX_PIN, TX_PIN);
 #define DebugPrintVln(x, y) ; 
 #endif
 
-
 // Routines to set and clear bits (used in the sleep code)
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -50,18 +49,10 @@ SoftwareSerial MySerial(RX_PIN, TX_PIN);
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-const long codel = 2180L / 4; // us
-//const int RF_TX_PIN = RF_TX_PIN; // the TxPin applied to the 433MHz modulator pin
-
 // EEPROM variables
-// counter kept in EEPROM to keep tack of the last sent code
-
-//uint16_e vrefl EEMEM; // 
-//uint16_e vrefr EEMEM; // 
-//uint8_e idx_pos1 EEMEM; // 
-//uint8_e idx_pos2 EEMEM; // 
-//uint8_e idx1t EEMEM; // 
-uint16_e ee_rfc_idx EEMEM; // 
+// counters kept in EEPROM to keep tack of the last sent code
+uint16_e ee_rfc_idx1 EEMEM; // 
+uint16_e ee_rfc_idx2 EEMEM; // 
 
 void transmitT(uint64_t message, uint8_t mlen, int wakeup = 11, int waitwake = 95)
 {
@@ -123,11 +114,83 @@ void transmitT(uint64_t message, uint8_t mlen, int wakeup = 11, int waitwake = 9
     delay(17); //17.2 exact
 }
 
+void transmitC(uint32_t message)
+{
+    const unsigned int syncup = 390;
+    const unsigned int syncdown = 430; //440;
+    const unsigned int pause_after_sync = 3600; //3720
+    const unsigned int per = 1233; //1248
+    const unsigned int oneup = 2 * per / 3; // 808 us exact
+    const unsigned int onedown = 1 * per / 3; // 440 us exact
+    const unsigned int zeroup = 1 * per / 3; // 393 us exact
+    const unsigned int zerodown = 2 * per / 3; // 855 us exact
+    const uint64_t prologue = 0b1100101011101110110010101110101111;
+
+    DebugPrint(F("T:"));
+    DebugPrintFln((long)message, HEX);
+
+    // send sync 101010101010101010101010
+    for (int i = 0; i < 12; i++)
+    {
+        digitalWrite(RF_TX_PIN, HIGH);
+        delayMicroseconds(syncup);
+        digitalWrite(RF_TX_PIN, LOW);
+        delayMicroseconds(syncdown);
+    }
+    delayMicroseconds(pause_after_sync);
+
+    // send content
+    uint32_t mask = 0x80000000;
+    while (mask)
+    {
+        if (message & mask)
+        {
+            // transmit 1 - long hi (810u) followed by shorter low (440us) (actually 0)
+            digitalWrite(RF_TX_PIN, HIGH);
+            delayMicroseconds(oneup);
+            digitalWrite(RF_TX_PIN, LOW);
+            delayMicroseconds(onedown);
+        }
+        else
+        {
+            // transmit 0 - short hi (395u) followed by long low (855us) (actually 1)
+            digitalWrite(RF_TX_PIN, HIGH);
+            delayMicroseconds(zeroup);
+            digitalWrite(RF_TX_PIN, LOW);
+            delayMicroseconds(zerodown);
+        }
+        mask >>= 1;
+    }
+
+    //    prologue = 0b1100101011101110110010101110101111;
+    uint64_t pmask = 0b1000000000000000000000000000000000;
+    while (pmask)
+    {
+        if (prologue & pmask)
+        {
+            // transmit 1 - long hi (810u) followed by shorter low (440us) 
+            digitalWrite(RF_TX_PIN, HIGH);
+            delayMicroseconds(oneup);
+            digitalWrite(RF_TX_PIN, LOW);
+            delayMicroseconds(onedown);
+        }
+        else
+        {
+            // transmit 0 - short hi (395u) followed by long low (855us)
+            digitalWrite(RF_TX_PIN, HIGH);
+            delayMicroseconds(zeroup);
+            digitalWrite(RF_TX_PIN, LOW);
+            delayMicroseconds(zerodown);
+        }
+        pmask >>= 1;
+    }
+    delay(16); //16.250 exact
+}
 
 // set system into the sleep state 
 // system wakes up when wtchdog is timed out
-void system_sleep() {
-
+void system_sleep() 
+{
     cbi(ADCSRA, ADEN);                    // switch Analog to Digital converter OFF
     digitalWrite(RF_TX_PIN, LOW);       // turn off modulator
 
@@ -139,10 +202,10 @@ void system_sleep() {
 }
 
 bool first_time = true;
+bool remote;
 int cnt = 0;
-uint16_t rfcode_idx = 0;
 uint64_t code;
-uint64_t codeP;
+uint32_t codeC;
 bool dip1, dip2;
 
 void setup()
@@ -153,40 +216,45 @@ void setup()
 
     DebugBegin(SERIAL_SPEED);
     DebugPrintln("");
-    DebugPrintV(F("idx was: "), ee_rfc_idx);
+    DebugPrintV(F("idx was: "), ee_rfc_idx1);
     DebugPrintVln(F(" / "), n_msgsP);
 
     dip1 = digitalRead(DIP1_PIN);
     dip2 = digitalRead(DIP2_PIN);
 
-    if(dip1) // if jumper1 off - advance to next code
-        ee_rfc_idx++;
-    // if jumper1 on - keep transmitting the same code
+    remote = dip1;
 
-    if (!dip2) // if jumper1 on - reset code counter to 0 - start codes list from begining
-        ee_rfc_idx = 0;
+    if (remote)
+    {
+        ee_rfc_idx1++; // advance to next code
+        if (ee_rfc_idx1 >= n_msgsP)
+            ee_rfc_idx1 = 0;
+        if (!dip2) // if jumper1 on - reset code counter to 0 - start codes list from begining
+            ee_rfc_idx1 = 0;
 
-    if (ee_rfc_idx >= n_msgsP)
-        ee_rfc_idx = 0;
+        DebugPrintVln("idx  is: ", ee_rfc_idx1);
 
-    DebugPrintVln("idx  is: ", ee_rfc_idx);
+        memcpy_P(&code, &msgsP[ee_rfc_idx1], sizeof(code));
 
-    //code = msgs[ee_rfc_idx];
-    memcpy_P(&codeP, &msgsP[ee_rfc_idx], sizeof(codeP));
-    code = codeP;
-        
-    DebugPrint(F("code:"));
-    DebugPrintF((long)(code>>32), HEX);
-    DebugPrintFln((long)code, HEX);
-    DebugPrint(F("codeP:"));
-    DebugPrintF((long)(codeP >> 32), HEX);
-    DebugPrintFln((long)codeP, HEX);
+        DebugPrint(F("code:"));
+        DebugPrintF((long)(code >> 32), HEX);
+        DebugPrintFln((long)code, HEX);
+    }
+    else
+    {
+        ee_rfc_idx2++; // advance to next code
+        if (ee_rfc_idx2 >= n_msgsC)
+            ee_rfc_idx2 = 0;
+        if (!dip2) // if jumper1 on - reset code counter to 0 - start codes list from begining
+            ee_rfc_idx2 = 0;
 
-    //rfcode_idx = ee_rfc_idx;
-    //Serial.print(F("t_size: "));
-    //Serial.print(n_msgsC);
-    //Serial.print(F(" "));
-    //Serial.println(n_msgs);
+        DebugPrintVln("idx  is: ", ee_rfc_idx2);
+
+        memcpy_P(&codeC, &msgsC[ee_rfc_idx2], sizeof(codeC));
+
+        DebugPrint(F("code:"));
+        DebugPrintFln((long)codeC, HEX);
+    }
 }
 
 uint8_t upcounter = 0xfe;
@@ -196,23 +264,30 @@ bool firsttime = true;
 
 void loop()
 {
-    upcounter++;
-    downcounter--;
-    uint64_t ins = (downcounter & 0b1100) | (upcounter & 0b11) | 0b00010000;
-
-    ins <<= 44;
-    if (firsttime)
+    if (remote)
     {
-        transmitT(code | ins, msglen);
-        firsttime = false;
+        upcounter++;
+        downcounter--;
+        uint64_t ins = (downcounter & 0b1100) | (upcounter & 0b11) | 0b00010000;
+
+        ins <<= 44;
+        if (firsttime)
+        {
+            transmitT(code | ins, msglen);
+            firsttime = false;
+        }
+        else
+        {
+            transmitT(code | ins, msglen, 0);
+        }
     }
     else
     {
-        transmitT(code | ins, msglen, 0);
+        transmitC(codeC);
     }
 
-    DebugPrintF(cnt,HEX);
     cnt++;
+    DebugPrintF(cnt, HEX);
 
     if (cnt > 15) // go to sleep after sending the code 16 times even if the button is still pressed
     {
